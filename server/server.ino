ffc921913d43ee92631367474550438726bc980c
Toma Bogdan <bogdantoma2000@gmail.com>
2024-06-20 17:50:53 +0300

    test - websocket.io

--- server/server.ino
+++ server/server.ino
@@ -1,12 +1,45 @@
 #include <ESP8266WiFi.h>
+#include <SocketIOclient.h>
+#include <WebSockets.h>
+#include <WebSockets4WebServer.h>
+#include <WebSocketsClient.h>
+#include <WebSocketsServer.h>
+#include <WebSocketsVersion.h>
+
 const char *ssid = "SolarSystem Wi-Fi";
 const char *password = "12344321";
 IPAddress local_IP(192, 168, 4, 22);
 IPAddress gateway(192, 168, 4, 9);
 IPAddress subnet(255, 255, 255, 0);
+
+WebSocketsServer webSocket = WebSocketsServer(81);
+
+void handleWebSocketEvent(uint8_t num, WStype_t type, uint8_t *payload, size_t length)
+{
+  switch (type)
+  {
+  case WStype_DISCONNECTED:
+    Serial.printf("[%u] Disconnected!\n", num);
+    break;
+  case WStype_CONNECTED:
+  {
+    IPAddress ip = webSocket.remoteIP(num);
+    Serial.printf("[%u] Connected from %s\n", num, ip.toString().c_str());
+    webSocket.sendTXT(num, "Connected");
+  }
+  break;
+  case WStype_TEXT:
+    Serial.printf("[%u] Received text: %s\n", num, payload);
+    webSocket.sendTXT(num, "Message received");
+    break;
+  }
+}
+
 void setup()
 {
-  Serial.begin(115200);
+  Serial.begin(9600);
+  delay(10);
+
   Serial.println();
   Serial.print("Setting soft-AP configuration ... ");
   Serial.println(WiFi.softAPConfig(local_IP, gateway, subnet) ? "Ready" : "Failed!");
@@ -17,10 +50,15 @@ void setup()
 
   Serial.print("Soft-AP IP address = ");
   Serial.println(WiFi.softAPIP());
+
+  webSocket.begin();
+  webSocket.onEvent(handleWebSocketEvent);
+  Serial.println('Websocket server started');
 }
 void loop()
 {
   Serial.print("[Server Connected] ");
-  Serial.println(WiFi.softAPIP());
+  /*Serial.println(WiFi.softAPIP());*/
+  webSocket.loop();
   delay(500);
 }
