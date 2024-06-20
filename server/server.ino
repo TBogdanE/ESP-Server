c57b15a4f0dce3ba458645c752facd0858af4d47
Toma Bogdan <bogdantoma2000@gmail.com>
2024-06-20 23:52:09 +0300

    test 1 - sending data to android

--- server/server.ino
+++ server/server.ino
@@ -1,19 +1,14 @@
 #include <ESP8266WiFi.h>
-#include <SocketIOclient.h>
-#include <WebSockets.h>
-#include <WebSockets4WebServer.h>
-#include <WebSocketsClient.h>
 #include <WebSocketsServer.h>
-#include <WebSocketsVersion.h>
 
-const char *ssid = "SolarSystem Wi-Fi";
-const char *password = "12344321";
-IPAddress local_IP(192, 168, 4, 22);
-IPAddress gateway(192, 168, 4, 9);
-IPAddress subnet(255, 255, 255, 0);
+// Replace with your network credentials
+const char *ssid = "TBE";
+const char *password = "Tbogdane2000";
 
+// Create a WebSocket server object on port 81
 WebSocketsServer webSocket = WebSocketsServer(81);
 
+// WebSocket event handler
 void handleWebSocketEvent(uint8_t num, WStype_t type, uint8_t *payload, size_t length)
 {
   switch (type)
@@ -25,9 +20,9 @@ void handleWebSocketEvent(uint8_t num, WStype_t type, uint8_t *payload, size_t l
   {
     IPAddress ip = webSocket.remoteIP(num);
     Serial.printf("[%u] Connected from %s\n", num, ip.toString().c_str());
-    webSocket.sendTXT(num, "Connected");
+    webSocket.sendTXT(num, "Connected now");
+    break;
   }
-  break;
   case WStype_TEXT:
     Serial.printf("[%u] Received text: %s\n", num, payload);
     webSocket.sendTXT(num, "Message received");
@@ -35,30 +30,43 @@ void handleWebSocketEvent(uint8_t num, WStype_t type, uint8_t *payload, size_t l
   }
 }
 
+int count = 0;
+
 void setup()
 {
+  // Start the Serial communication to send messages to the computer
   Serial.begin(9600);
-  delay(10);
 
-  Serial.println();
-  Serial.print("Setting soft-AP configuration ... ");
-  Serial.println(WiFi.softAPConfig(local_IP, gateway, subnet) ? "Ready" : "Failed!");
-  Serial.print("Setting soft-AP ... ");
-  Serial.println(WiFi.softAP(ssid, password) ? "Ready" : "Failed!");
-  // WiFi.softAP(ssid);
-  // WiFi.softAP(ssid, password, channel, hidden, max_connection)
+  // Connect to Wi-Fi
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(1000);
+    Serial.println("Connecting to WiFi...");
+  }
 
-  Serial.print("Soft-AP IP address = ");
-  Serial.println(WiFi.softAPIP());
+  Serial.println("Connected to WiFi");
+  Serial.print("IP Address: ");
+  Serial.println(WiFi.localIP());
 
+  // Start WebSocket server
   webSocket.begin();
   webSocket.onEvent(handleWebSocketEvent);
-  Serial.println('Websocket server started');
+  Serial.println("WebSocket server started");
 }
+
 void loop()
 {
-  Serial.print("[Server Connected] ");
-  /*Serial.println(WiFi.softAPIP());*/
-  webSocket.loop();
-  delay(500);
+  webSocket.loop(); // Handle WebSocket events
+
+  // Send count to connected clients every 2 seconds
+  static unsigned long lastSend = 0;
+  unsigned long now = millis();
+  if (now - lastSend >= 2000)
+  {
+    lastSend = now;
+    count++;
+    String message = "count:" + String(count);
+    webSocket.broadcastTXT(message.c_str());
+  }
 }
